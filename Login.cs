ff7c1ab63cddac29cd6ab2265f7c8d7410979895
Ayushmane009 <ayushmane009@gmail.com>
2025-09-13 21:34:04 +0530

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DCMS.Admin
+{
+    public partial class Login : Form
+    {
+        SqlConnection cn = new SqlConnection(@" ");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        SqlDataReader dr;
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        String strusername, strpassword;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlogin1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
